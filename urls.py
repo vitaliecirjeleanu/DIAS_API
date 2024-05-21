f5a9c561c78ce58b91fba6e8dc892ed644ecb18b
Vitalie <vitalie.cirjeleanu@gmail.com>
2024-05-21 19:04:35 +0300

    updates related to railway

--- urls.py
+++ urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/topics/', include('topics.urls')),
     path('api/int-football/', include('int_football.urls')),
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
