b62c2fc98f11cac6fcc0aae52cb6380de1e9af6b
Vitalie <vitalie.cirjeleanu@gmail.com>
2024-05-24 14:12:24 +0300

    railway updates

--- utils/image_upload.py
+++ utils/image_upload.py
@@ -7,22 +7,26 @@ from rest_framework.response import Response
 
 class ImageUploadView(APIView):
     def post(self, request):
-        serializer = ImageUploadSerializer(data=request.data)
+        try:
+            serializer = ImageUploadSerializer(data=request.data)
         
-        if serializer.is_valid():
-            try:
+            if serializer.is_valid():
                 image, dir_name = serializer.validated_data.values()
                 new_dir = os.path.join(settings.MEDIA_ROOT, 'images', dir_name)
 
                 if not os.path.exists(new_dir):
                     os.makedirs(new_dir, exist_ok=True)  
+                path = os.path.join(new_dir, image.name) 
+
+                with open(path, 'wb+') as destination:
+                    for chunk in image.chunks():
+                        destination.write(chunk)
 
                 return Response({"message": "Image uploaded successfully"}, status=status.HTTP_201_CREATED)
-            except Exception as e:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except Exception as e:
                 return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-        
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
+    
 
 class ImageUploadSerializer(serializers.Serializer):
     image = serializers.ImageField()
